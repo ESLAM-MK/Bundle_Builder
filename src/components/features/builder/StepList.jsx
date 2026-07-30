13d17b8fc14b54744cac81322f621e70fd917daa
Eslam Medhat Kamel <medhateslam886@gmail.com>
2026-07-30 15:54:21 +0300

    update data to be available in deployment

--- src/components/features/builder/StepList.jsx
+++ src/components/features/builder/StepList.jsx
@@ -4,19 +4,20 @@ import { useSelector } from 'react-redux';
 import StepIcon from '../../Ui/StepIcon';
 import fetchFun from '../../../utils/fetchFun.js';
 import { useQuery } from '@tanstack/react-query';
+import {steps} from "../../../data/items.json"
 import { v4 as uuidv4 } from "uuid"
-const fetchSteps = async () => {
-  return await fetchFun('http://localhost:3000/steps')
-}
+// const fetchSteps = async () => {
+//   return await fetchFun('http://localhost:3000/steps')
+// }
 const StepList = () => {
   const openSteps = useSelector((state) => state.bundle.openSteps)
   const categories = useSelector(state => state.bundle.categories)
-  const { data: steps, isLoading, isError, error } = useQuery({
-    queryKey: ['steps'],
-    queryFn: fetchSteps
-  })// fetching data from server using tanstack query 
-  if (isLoading) return <div>Loading...</div> // for loading
-  if (isError) return <div>Error: {error.message}</div> // appears in error
+//   const { data: steps, isLoading, isError, error } = useQuery({
+//     queryKey: ['steps'],
+//     queryFn: fetchSteps
+//   })// fetching data from server using tanstack query 
+//   if (isLoading) return <div>Loading...</div> // for loading
+//   if (isError) return <div>Error: {error.message}</div> // appears in error
   return (
     <div className="w-full flex flex-col ">
       {steps.map((step) => {
